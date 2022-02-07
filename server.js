2de29a6666e09bee3c93151f5d0a1571997a6166
Austin Miller <austin.miller1424@gmail.com>
2022-02-07 14:31:45 -0600

    added query and delet functions

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mysql = require('mysql2');
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'SMUcoding2022',
+        database:'election'
+    },
+    console.log('connect to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+db.query(`SELECT * FROM candidates WHERE id=1`, (err,row)=>{
+    if (err){
+        console.log(err);
+    }
+    console.log(row);
+})
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     }
+//     console.log(result);
+//   });
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+  });
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
